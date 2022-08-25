b0d1a9a9ff0aba73c00858da595de562aace531e
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-25 11:14:52 -0500

    first part of sql ex

---
+++ albums_migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE albums (
+                        id INT (11) NOT NULL AUTO_INCREMENT,
+                        artist_name VARCHAR(100),
+                        album_name  VARCHAR(100),
+                        release_date INT(4),
+                        sales DOUBLE PRECISION,
+                        genre VARCHAR(100),
+                        PRIMARY KEY (id)
+);

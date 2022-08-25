4b740046e3a8c5f438be3f20b0f8ca1c64d6988f
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-25 16:55:02 -0500

    completed update ex

---
+++ update_exercises.sql
@@ -0,0 +1,27 @@
+-- Write SELECT statements to output each of the following with a caption:
+--     After each SELECT add an UPDATE statement:
+--     Add SELECT statements after each UPDATE so you can see the results of your handiwork.
+
+
+SELECT album_name FROM albums;
+--     UPDATE statement to make all the albums 10 times more popular (sales * 10)
+UPDATE albums
+SET sales = sales * 10;
+
+--     All albums released before 1980
+SELECT album_name FROM albums
+WHERE release_date < '1980';
+--     Move all the albums before 1980 back to the 1800s.
+UPDATE albums
+SET release_date = '1800'
+WHERE release_date < '1980';
+
+--     All albums by Michael Jackson
+SELECT album_name FROM albums
+WHERE artist_name = 'Michael Jackson';
+--     Change 'Michael Jackson' to 'Peter Jackson'
+UPDATE albums
+SET  artist_name = 'Peter Jackson'
+WHERE artist_name = 'Michael Jackson';
+
+

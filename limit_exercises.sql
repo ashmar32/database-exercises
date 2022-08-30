1a467ec7c5a9eb061dd9c9febf8363d3109e3dbc
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-30 14:05:13 -0500

    starting limit ex

---
+++ limit_exercises.sql
@@ -0,0 +1,3 @@
+--MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. For example, to find all the unique titles within the company, we could run the following query:
+
+SELECT DISTINCT title FROM titles;

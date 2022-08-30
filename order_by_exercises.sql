1c6b0d1ab2b063c872919f1c45c189ae702a9899
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-30 13:49:01 -0500

    completed order ex

---
+++ order_by_exercises.sql
@@ -0,0 +1,48 @@
+-- 2. Modify your first query to order by first name. The first result should be Irena Pelz and the last result should be Vidya Awdeh.
+
+/*
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name ASC;
+*/
+
+-- 3. Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+
+/*
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+*/
+
+-- 4. Change the ORDER BY clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+
+/*
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+*/
+
+-- 5. Update your queries for employees with 'e' in their last name to sort the results by their employee number. Make sure the employee numbers are in the correct order.
+
+/*
+SELECT e.*
+FROM employees as e
+WHERE e.last_name LIKE '%e%'
+ORDER BY e.emp_no;
+ */
+
+-- 6. Now reverse the sort order for both queries and compare results.
+/*
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name DESC;
+
+SELECT *
+FROM employees
+WHERE last_name like '%e%'
+ORDER BY emp_no DESC;
+ */
--- where_exercises.sql
+++ where_exercises.sql
@@ -1,53 +1,55 @@
-# USE employees
+-- USE employees
 
-# 2. Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+/*
+2. Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
 
-# SELECT first_name
-# FROM employees
-# WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-# 3. Find all employees whose last name starts with 'E' — 7,330 rows.
+3. Find all employees whose last name starts with 'E' — 7,330 rows.
 
-# SELECT last_name
-# FROM employees
-# WHERE last_name LIKE 'E%';
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
 
-# 4. Find all employees with a 'q' in their last name — 1,873 rows.
+4. Find all employees with a 'q' in their last name — 1,873 rows.
 
-# SELECT last_name
-# FROM employees
-# WHERE last_name LIKE '%q%'
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
 
-# Part 2
-# 1. Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+Part 2
+1. Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
 
-# SELECT first_name
-# FROM employees
-# WHERE first_name IN ('Irena', 'Vidya')
-# OR first_name = 'Maya';
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya')
+OR first_name = 'Maya';
 
-# 2. Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+2. Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
 
-# SELECT first_name
-# FROM employees
-# WHERE first_name IN ('Irena', 'Vidya')
-# AND gender = 'male'
-# OR first_name = 'Maya';
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya')
+AND gender = 'male'
+OR first_name = 'Maya';
 
-# 3. Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-# SELECT *
-# FROM employees
-# WHERE last_name LIKE 'e%'
-# OR last_name LIKE '%e';
+3. Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+OR last_name LIKE '%e';
 
-# 4. Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
-# SELECT *
-# FROM employees
-# WHERE last_name LIKE 'e%'
-# AND last_name LIKE '%e';
+4. Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+AND last_name LIKE '%e';
 
-# 5. Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+5. Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%'
 OR last_name LIKE '%qu%'
+ */

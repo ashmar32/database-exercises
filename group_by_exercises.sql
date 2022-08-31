964d07114d304b7c592e6b87ae020c7c2b076f5c
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-31 15:58:26 -0500

    completed and reviewed groupby ex

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,7 +2,7 @@
 
 /*
 SELECT DISTINCT title
-FROM employees.titles;
+FROM titles;
 */
 
 -- 3. Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
